3132e5b10a06da9277ca8e6ee8f700ae4ba72a00
高見知英 <chie@onpu-tamago.net>
2015-03-17 21:30:38 +0900

    Process起動時にエラーが発生したときの処理追加

--- CUIToolHelper/CUIToolHelper/Program.cs
+++ CUIToolHelper/CUIToolHelper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,23 +19,34 @@ namespace TakamiChie.CUIToolHelper
         [STAThread]
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            if (args.Length > 0 && File.Exists(args[0]))
             {
                 ProcessStartInfo pi = new ProcessStartInfo(args[0]);
                 if (args.Length > 1) { pi.Arguments = string.Join(" ", args.Skip(1)); }
                 pi.RedirectStandardOutput = true;
                 pi.RedirectStandardError = true;
                 pi.UseShellExecute = false;
-                // プロセス実行
-                var proc = Process.Start(pi);
-                proc.WaitForExit();
-                var output = proc.StandardOutput.ReadToEnd();
-                var errors = proc.StandardError.ReadToEnd();
-                var oe = output != "";
-                var ee = errors != "";
-                var displaymsg = oe ? 
-                    (ee ? output + "\nエラー：" + errors : output) : 
-                    (ee ? errors : "");
+                var oe = false;
+                var ee = false;
+                var displaymsg = "";
+                try
+                {
+
+                    // プロセス実行
+                    var proc = Process.Start(pi);
+                    proc.WaitForExit();
+                    var output = proc.StandardOutput.ReadToEnd();
+                    var errors = proc.StandardError.ReadToEnd();
+                    oe = output != "";
+                    ee = errors != "";
+                    displaymsg = oe ?
+                                        (ee ? output + "\nエラー：" + errors : output) :
+                                        (ee ? errors : "");
+                }
+                catch (Exception e)
+                {
+                    displaymsg = e.Message;
+                }
                 if (displaymsg != "")
                 {
                     MessageBox.Show(displaymsg, oe ? "情報" : "エラー", MessageBoxButtons.OK,
