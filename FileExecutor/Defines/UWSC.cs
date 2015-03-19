c3479f9a811db0159d4c5be2cae99b7fd250b092
高見知英 <chie@onpu-tamago.net>
2015-03-19 20:15:22 +0900

    UWSCを追加

---
+++ FileExecutor/Defines/UWSC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakamiChie.FileExecutor.Defines
+{
+    [DefaultFileExt(".uws")]
+    class UWSC : BasicFileType
+    {
+        protected override string executor
+        {
+            get { return "UWSC"; }
+        }
+    }
+}
