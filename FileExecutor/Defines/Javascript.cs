642528a4b0c1b1fb2539b1e4f39f92a2dc01412c
高見知英 <chie@onpu-tamago.net>
2015-06-17 18:23:24 +0900

    jsファイルを開く場合、ファイル名の前後に余計なダブルクオーテーションをつけていた問題対処

--- FileExecutor/Defines/Javascript.cs
+++ FileExecutor/Defines/Javascript.cs
@@ -33,7 +33,7 @@ namespace TakamiChie.FileExecutor.Defines
             else
             {
                 // ファイルを移動しない
-                newfn = '"' + oldfn + '"';
+                newfn = oldfn;
             }
             this.filename = newfn;
 
