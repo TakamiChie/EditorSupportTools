e22b2ddd75d772236fd010a990b5f2702fa6d67c
高見知英 <chie@onpu-tamago.net>
2015-09-18 22:15:37 +0900

    CommandRunnerを作成

---
+++ CommandRunner/CommandManager.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TakamiChie.CommandRunner
+{
+  class CommandManager
+  {
+    private Dictionary<String, String> map;
+    private Dictionary<String, String> pathMap;
+
+    /// <summary>
+    /// コストラクタ
+    /// </summary>
+    /// <param name="path">対象となるファイルのパス</param>
+    public CommandManager(String path)
+    {
+      this.map = new Dictionary<string, string>();
+      this.pathMap = new Dictionary<string, string>();
+      pathMapping(path);
+      codeMapping(path);
+    }
+
+    /// <summary>
+    /// 値がコマンドリストに存在するかどうか
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <returns>キーが存在すればtrue</returns>
+    public bool hasKey(String key)
+    {
+      return map.ContainsKey(key);
+    }
+
+    /// <summary>
+    /// コマンドリストの値を取得する
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <returns>キー内の値</returns>
+    public string get(String key)
+    {
+      return map[key];
+    }
+
+    /// <summary>
+    /// コマンドリストの値を取得。実行ファイルが見つかればそのパスを取得する
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <returns>キー内の値</returns>
+    public string getPath(String key)
+    {
+      return which(get(key));
+    }
+
+    /// <summary>
+    /// コマンドリストの値を取得。パス変数を展開する
+    /// </summary>
+    /// <param name="key">キー</param>
+    /// <returns>キー内の値</returns>
+    public string getExtractVar(String key)
+    {
+      var s = get(key);
+      foreach (var item in pathMap)
+      {
+        s = s.Replace(item.Key, item.Value);
+      }
+      return s;
+    }
+
+    /// <summary>
+    /// パス変数の一覧を生成する
+    /// </summary>
+    /// <param name="path">対象となるファイルのパス</param>
+    private void pathMapping(String path)
+    {
+      this.pathMap.Add("$1", path);
+      this.pathMap.Add("$dn1", Path.GetDirectoryName(path));
+      this.pathMap.Add("$fn1", Path.GetFileName(path));
+      this.pathMap.Add("$ex1", Path.GetExtension(path));
+      this.pathMap.Add("$fnwe1", Path.GetFileNameWithoutExtension(path));
+    }
+
+    /// <summary>
+    /// コード変数の一覧を生成する
+    /// </summary>
+    /// <param name="path">対象となるファイルのパス</param>
+    private void codeMapping(String path)
+    {
+      var regex = new Regex(@".*\*{2}\s*(\w+):(.*)");
+      using (var stream = new StreamReader(path, Encoding.Default))
+      {
+        string line;
+        while(stream.Peek() >= 0)
+        {
+          line = stream.ReadLine();
+          var m = regex.Match(line);
+          if (m.Success)
+          {
+            this.map.Add(m.Groups[1].Value, m.Groups[2].Value);
+          }
+          else
+          {
+            break;
+          }
+        }
+      }
+    }
+
+    /// <summary>
+    /// ファイル名をパス名に変換する
+    /// </summary>
+    /// <param name="fileName">ファイル名</param>
+    /// <returns>パス</returns>
+    private string which(String fileName)
+    {
+      return Environment.GetEnvironmentVariable("PATH").Split(';')
+        .Select(x => Path.Combine(x, fileName))
+        .SelectMany(_ => Environment.GetEnvironmentVariable("PATHEXT").Split(';')
+          .Concat(new String[] { Path.GetExtension(fileName) }),
+            (p, ext) => Path.ChangeExtension(p, ext))
+        .Where(File.Exists).FirstOrDefault();
+    }
+  }
+}
---
+++ CommandRunner/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TakamiChie.CommandRunner
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /// コマンドラインオプション解析
+      // http://neue.cc/2009/12/13_229.html        }
+      var options = new HashSet<string> { "/file" };
+
+      string key = null;
+      var result = args
+          .GroupBy(s => options.Contains(s) ? key = s : key)
+          .ToDictionary(g => g.Key, g => g.Skip(1).FirstOrDefault());
+
+      try
+      {
+
+        var path = String.Empty;
+        if (result.TryGetValue("/file", out path))
+        {
+          if (!Path.IsPathRooted(path))
+          {
+            path = Path.Combine(System.Environment.CurrentDirectory, path);
+          }
+          var manager = new CommandManager(path);
+          if (manager.hasKey("run"))
+          {
+            // プロセス起動準備
+            var procinfo = new ProcessStartInfo(manager.getPath("run"));
+            procinfo.Arguments = manager.hasKey("args") ? manager.getExtractVar("args") : "";
+            procinfo.CreateNoWindow = true;
+            procinfo.RedirectStandardOutput = true;
+            procinfo.RedirectStandardError = true;
+            procinfo.UseShellExecute = false;
+            var stdout = "";
+            var stderr = "";
+            try
+            {
+              // 起動
+              var proc = Process.Start(procinfo);
+              stdout = proc.StandardOutput.ReadToEnd();
+              stderr = proc.StandardError.ReadToEnd();
+
+              // 結果表示
+              switch (manager.hasKey("out") ? manager.get("out") : "console")
+              {
+                case "file":
+                  // ファイル出力
+                  if (manager.hasKey("outfile"))
+                  {
+                    using (var stream = new StreamWriter(manager.getExtractVar("outfile")))
+                    {
+                      stream.Write(stdout);
+                    }
+                  }
+                  else
+                  {
+                    throw new InvalidDataException("outfileコマンドが見つかりません");
+                  }
+                  break;
+                case "console":
+                  Console.Out.Write(stdout);
+                  break;
+                case "dialog":
+                  MessageBox.Show(stdout);
+                  break;
+              }
+            }
+            catch (Exception e)
+            {
+              stderr = e.Message;
+            }
+            if (stderr != "")
+            {
+              Console.Error.Write(stderr);
+            }
+
+            // メッセージ表示
+            if (manager.hasKey("console"))
+            {
+              Console.Write(manager.getExtractVar("console"));
+            }
+
+            if (manager.hasKey("dialog"))
+            {
+              MessageBox.Show(manager.getExtractVar("dialog"));
+            }
+          }
+        }
+        else
+        {
+          throw new InvalidOperationException("Command Line Error:\nCommandRunner /file [FileName]");
+        }
+      }
+      catch (Exception e)
+      {
+        Console.Error.WriteLine(e.Message);
+      }
+    }
+  }
+}
